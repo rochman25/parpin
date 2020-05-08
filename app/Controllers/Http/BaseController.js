0eddc46f11082757e0482d4208352abf1393f419
rochman25 <zaenur.rochman98@gmail.com>
2020-05-09 01:53:22 +0700

    create base controllers for backend

---
+++ app/Controllers/Http/BaseController.js
@@ -0,0 +1,48 @@
+'use strict'
+
+class BaseController {
+    constructor() {
+        this.welcomeMessage = "Hai from API Parpin";
+
+        this.errorMessage = "Something not right";
+
+        this.validationMessage = "Validation Error.";
+
+        this.addSuccessMessage = "Insert data succes.";
+        this.updateSuccessMessage = "Update data success.";
+
+        this.dataFound = "Data Found.";
+        this.dataNotFound = "Data Not Found.";
+        this.emailNotFound = "Email Not Found.";
+
+        this.loginSuccessMessage = "Login success.";
+        this.loginFailedMessage = "Sorry you can't login with this account.";
+
+        this.registerSuccessMessage = "Register Success.";
+    }
+
+
+    successResponse(respon = "") {
+        let message = (typeof respon.message != "undefined") ? respon.message : this.welcomeMessage;
+        let statusCode = 200;
+        let data = (typeof respon.data != "undefined") ? respon.data : [];
+        return {
+            code: statusCode,
+            message: message,
+            data: data
+        };
+    }
+
+    errorResponse(respon = "") {
+        let message = (typeof respon.message != "undefined") ? respon.message : this.errorMessage;
+        let statusCode = 401;
+        let data = (typeof respon.error != "undefined") ? respon.error : [];
+        return {
+            code: statusCode,
+            message: message,
+            error: data
+        };
+    }
+}
+
+module.exports = BaseController
---
+++ app/Controllers/Http/PenggunaController.js
@@ -0,0 +1,17 @@
+'use strict'
+const BaseController = use('App/Controllers/Http/BaseController.js')
+
+class PenggunaController extends BaseController {
+
+    async login_action({ request, response, auth }) {
+
+    }
+
+    async register_action({ request, response }) {
+
+    }
+
+
+}
+
+module.exports = PenggunaController
