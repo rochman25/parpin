5339053dc30326cc7027d6135660d3ef7a1545f9
rochman25 <zaenur.rochman98@gmail.com>
2020-05-15 02:06:17 +0700

    add notifikasi controller

---
+++ app/Controllers/Http/NotifikasiController.js
@@ -0,0 +1,13 @@
+'use strict'
+const BaseController = use("App/Controllers/Http/BaseController.js");
+const Notifikasi = use("App/Models/Notifikasi");
+
+class NotifikasiController extends BaseController {
+
+    async index({ request, response }) {
+
+    }
+
+}
+
+module.exports = NotifikasiController
