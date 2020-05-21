277be0b0be5358f72043a19c85660b415bf086ff
rochman25 <zaenur.rochman98@gmail.com>
2020-05-21 11:17:13 +0700

    update halaman notifikasi

--- public/13.js
+++ public/13.js
@@ -1,5 +1,58 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[13],{
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=script&lang=js&":
+/*!**********************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/babel-loader/lib??ref--10-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=script&lang=js& ***!
+  \**********************************************************************************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      val: false
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=template&id=c46799ac&":
 /*!*******************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=template&id=c46799ac& ***!
@@ -15,7 +68,101 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div")
+  return _c("vx-card", { attrs: { title: "Notifikasi Alat" } }, [
+    _c("p", [
+      _vm._v(
+        "You can add custom content to the item. It will be pushed to the right side"
+      )
+    ]),
+    _vm._v(" "),
+    _c(
+      "div",
+      { staticClass: "demo-alignment" },
+      [
+        _c(
+          "vs-list",
+          [
+            _c("vs-list-header", { attrs: { title: "Group 1" } }),
+            _vm._v(" "),
+            _c(
+              "vs-list-item",
+              {
+                attrs: {
+                  title: "Snickerdoodle",
+                  subtitle: "An excellent companion"
+                }
+              },
+              [
+                _c("vs-button", { attrs: { color: "danger" } }, [
+                  _vm._v("One action")
+                ])
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "vs-list-item",
+              {
+                attrs: {
+                  title: "Sapporo Haru",
+                  subtitle:
+                    "An excellent polish restaurant, quick delivery and hearty, filling meals"
+                }
+              },
+              [_c("vs-checkbox", { attrs: { color: "danger" } })],
+              1
+            ),
+            _vm._v(" "),
+            _c("vs-list-header", {
+              attrs: { title: "Group 2", color: "success" }
+            }),
+            _vm._v(" "),
+            _c(
+              "vs-list-item",
+              {
+                attrs: {
+                  title: "Enid's",
+                  subtitle:
+                    "At night a bar, during the day a delicious brunch spot."
+                }
+              },
+              [
+                _c("vs-chip", { attrs: { color: "warning mr-0" } }, [
+                  _vm._v("Another component")
+                ])
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "vs-list-item",
+              {
+                attrs: {
+                  title: "Veronika Ossi",
+                  subtitle: "Has not watched anything recently"
+                }
+              },
+              [
+                _c("vs-switch", {
+                  attrs: { color: "warning" },
+                  model: {
+                    value: _vm.val,
+                    callback: function($$v) {
+                      _vm.val = $$v
+                    },
+                    expression: "val"
+                  }
+                })
+              ],
+              1
+            )
+          ],
+          1
+        )
+      ],
+      1
+    )
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -34,15 +181,17 @@ render._withStripped = true
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _view_notifikasi_vue_vue_type_template_id_c46799ac___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./view_notifikasi.vue?vue&type=template&id=c46799ac& */ "./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=template&id=c46799ac&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _view_notifikasi_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./view_notifikasi.vue?vue&type=script&lang=js& */ "./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _view_notifikasi_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _view_notifikasi_vue_vue_type_template_id_c46799ac___WEBPACK_IMPORTED_MODULE_0__["render"],
   _view_notifikasi_vue_vue_type_template_id_c46799ac___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -56,6 +205,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=script&lang=js&":
+/*!*******************************************************************************************************!*\
+  !*** ./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_babel_loader_lib_index_js_ref_10_0_node_modules_vue_loader_lib_index_js_vue_loader_options_view_notifikasi_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../../../node_modules/babel-loader/lib??ref--4-0!../../../../../../node_modules/babel-loader/lib??ref--10-0!../../../../../../node_modules/vue-loader/lib??vue-loader-options!./view_notifikasi.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_babel_loader_lib_index_js_ref_10_0_node_modules_vue_loader_lib_index_js_vue_loader_options_view_notifikasi_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue
+++ resources/assets/frontend/views/pages/Notifikasi/view_notifikasi.vue
@@ -1,3 +1,43 @@
 <template>
-    
+  <vx-card title="Notifikasi Alat">
+    <p>You can add custom content to the item. It will be pushed to the right side</p>
+
+    <div class="demo-alignment">
+      <vs-list>
+        <vs-list-header title="Group 1"></vs-list-header>
+
+        <vs-list-item title="Snickerdoodle" subtitle="An excellent companion">
+          <vs-button color="danger">One action</vs-button>
+        </vs-list-item>
+
+        <vs-list-item
+          title="Sapporo Haru"
+          subtitle="An excellent polish restaurant, quick delivery and hearty, filling meals"
+        >
+          <vs-checkbox color="danger" />
+        </vs-list-item>
+
+        <vs-list-header title="Group 2" color="success"></vs-list-header>
+        <vs-list-item
+          title="Enid's"
+          subtitle="At night a bar, during the day a delicious brunch spot."
+        >
+          <vs-chip color="warning mr-0">Another component</vs-chip>
+        </vs-list-item>
+
+        <vs-list-item title="Veronika Ossi" subtitle="Has not watched anything recently">
+          <vs-switch color="warning" v-model="val" />
+        </vs-list-item>
+      </vs-list>
+    </div>
+  </vx-card>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      val: false
+    };
+  }
+};
+</script>
