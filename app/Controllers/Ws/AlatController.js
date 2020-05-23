51b301129a933efcd24a9765731b59614f4463ba
rochman25 <zaenur.rochman98@gmail.com>
2020-05-23 22:36:34 +0700

    add sample function

--- app/Controllers/Ws/AlatController.js
+++ app/Controllers/Ws/AlatController.js
@@ -1,10 +1,15 @@
 'use strict'
 
 class AlatController {
-  constructor ({ socket, request }) {
-    this.socket = socket
-    this.request = request
-  }
+    constructor({ socket, request }) {
+        this.socket = socket
+        this.request = request
+    }
+
+    onMessage(message) {
+        this.socket.broadcastToAll('message', message);
+    }
+
 }
 
 module.exports = AlatController
