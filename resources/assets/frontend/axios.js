68543f627e613db75eb734769bac914ff2dbd44d
rochman25 <zaenur.rochman98@gmail.com>
2020-07-13 23:50:42 +0700

    update axios

--- resources/assets/frontend/axios.js
+++ resources/assets/frontend/axios.js
@@ -8,7 +8,8 @@ export default axios.create({
     domain,
     // baseURL: 'http://192.168.1.8:3333/',
     // baseURL: 'http://192.168.43.73:3333/',
-    baseURL: 'http://127.0.0.1:3333/',
+    // baseURL: 'http://127.0.0.1:3333/',
+    baseURL: 'http://parpin.herokuapp.com',
     headers: {
         'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
     }
